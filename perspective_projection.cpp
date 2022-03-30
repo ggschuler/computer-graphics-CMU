5682b5a3179a152e2c53c2ac7ad4c4c0ced5bd3f
Guilherme Schüler <51725262+ggschuler@users.noreply.github.com>
2022-03-30 16:12:39 -0300

    Create perspective_projection.cpp
    just gibberish code for later work.

---
+++ perspective_projection.cpp
@@ -0,0 +1,23 @@
+int alg(int vertXYZ) {
+  xyz = vertXYZ - camera;
+  u = vertX/z;
+  v = vertY/z;
+}
+
+int drawLine() {
+  //between (u1,v1) and (u2,v2)
+}
+
+int main() {
+
+  int camera = (2,3,5);
+  
+  int vertA = (1,1,1);
+  int vertB = (-1,1,1);
+  int vertC = (1,-1,1);
+  int vertD = (-1,-1,1);
+  int vertE = (1,1,-1);
+  int vertF = (-1,1,-1);
+  int vertG = (1,-1,-1);
+  int vertH = (-1,-1,-1);
+}
